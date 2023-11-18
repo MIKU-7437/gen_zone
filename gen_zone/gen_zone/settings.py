b59f67f53aae31b571474a7a93042b34a86adc7b
Miku <nurshanov.dias@gmail.com>
2023-11-18 20:31:36 +0600

    host all

--- gen_zone/gen_zone/settings.py
+++ gen_zone/gen_zone/settings.py
@@ -27,13 +27,15 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1))
 
+# ALLOWED_HOSTS = [
+#     'inquisitive-creature-production.up.railway.app',
+#     'inquisitive-creature-development.up.railway.app',
+#     'localhost',
+#     '127.0.0.1',
+# ]
 ALLOWED_HOSTS = [
-    'inquisitive-creature-production.up.railway.app',
-    'inquisitive-creature-development.up.railway.app',
-    'localhost',
-    '127.0.0.1',
+    '*',
 ]
-
 
 
 # Application definition
