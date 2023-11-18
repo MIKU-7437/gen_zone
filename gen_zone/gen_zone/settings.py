4c3812d41e38309aeffa56909f8c6328542d7904
Miku <nurshanov.dias@gmail.com>
2023-11-18 20:48:54 +0600

    host all v3

--- gen_zone/gen_zone/settings.py
+++ gen_zone/gen_zone/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'gen_zone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -108,16 +108,16 @@ WSGI_APPLICATION = 'gen_zone.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'railway'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '35-*D63DEBC4bFB62F5gc*4E4-c5ef*d'),
-        'PORT': os.environ.get('POSTGRES_PORT', 32441),
-        'HOST': os.environ.get('POSTGRES_HOST', 'roundhouse.proxy.rlwy.net'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB', 'railway'),
+#         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '35-*D63DEBC4bFB62F5gc*4E4-c5ef*d'),
+#         'PORT': os.environ.get('POSTGRES_PORT', 32441),
+#         'HOST': os.environ.get('POSTGRES_HOST', 'roundhouse.proxy.rlwy.net'),
+#     }
+# }
 
 
 # Password validation
@@ -178,11 +178,17 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 CORS_ALLOWED_ORIGINS = [
-    '*'
+    'https://inquisitive-creature-production.up.railway.app',
+    'https://inquisitive-creature-development.up.railway.app',
+    'http://localhost:5173',
+    'https://localhost:5173',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    '*'
+    'https://inquisitive-creature-production.up.railway.app',
+    'https://inquisitive-creature-development.up.railway.app',
+    'http://localhost:5173',
+    'https://localhost:5173',
 ]
 
 #
