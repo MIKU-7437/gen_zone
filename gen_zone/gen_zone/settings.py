024f25338a8c2ce2aa5c753ca5e3927b53215ca2
Miku <nurshanov.dias@gmail.com>
2023-11-18 20:35:48 +0600

    host all v2

--- gen_zone/gen_zone/settings.py
+++ gen_zone/gen_zone/settings.py
@@ -178,13 +178,11 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 CORS_ALLOWED_ORIGINS = [
-    'https://inquisitive-creature-production.up.railway.app',
-    'https://inquisitive-creature-development.up.railway.app',
+    '*'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://inquisitive-creature-production.up.railway.app',
-    'https://inquisitive-creature-development.up.railway.app',
+    '*'
 ]
 
 #
